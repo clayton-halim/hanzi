c9ae99995e9e83488d76bfa953106ee52a1e93d3
Clayton Halim <claytonhalim3@gmail.com>
2019-10-05 01:44:16 -0400

    Add test changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import HanziRow from "./components/HanziRow";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Hanzi Worksheets</h1>
+        <HanziRow boxes={18} character="分" />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
