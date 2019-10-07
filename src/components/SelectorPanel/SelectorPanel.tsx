8750029e3b4202916ac67c5d0ad3847636d2591e
Clayton Halim <claytonhalim3@gmail.com>
2019-10-07 10:12:54 -0400

    Add filter view

--- src/components/SelectorPanel/SelectorPanel.tsx
+++ src/components/SelectorPanel/SelectorPanel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Selector from "../Selector";
 import { HanziData } from "../../types";
@@ -9,16 +9,69 @@ export type Props = {
   onClick: (selected: boolean, hanzi: HanziData) => void;
 };
 
+type FilterView =
+  | "all"
+  | "freq"
+  | "hsk1"
+  | "hsk2"
+  | "hsk3"
+  | "hsk4"
+  | "hsk5"
+  | "hsk6";
+
+function filterData(data: HanziData[], view: FilterView) {
+  switch (view) {
+    case "all":
+      return data;
+    case "freq":
+      return [...data].sort(entry => entry.freq_rank);
+    case "hsk1":
+      return data.filter(entry => entry.hsk === 1);
+    case "hsk2":
+      return data.filter(entry => entry.hsk === 2);
+    case "hsk3":
+      return data.filter(entry => entry.hsk === 3);
+    case "hsk4":
+      return data.filter(entry => entry.hsk === 4);
+    case "hsk5":
+      return data.filter(entry => entry.hsk === 5);
+    case "hsk6":
+      return data.filter(entry => entry.hsk === 6);
+  }
+}
+
 const SelectorPanel: React.FC<Props> = ({ data, onClick }: Props) => {
+  const [view, setView] = useState<FilterView>("all");
+
+  const handleChange = (event: React.FormEvent<HTMLSelectElement>) =>
+    setView(event.currentTarget.value as FilterView);
+
+  const filteredData = filterData(data, view);
+
   return (
     <div className="selector-panel">
-      {data.map((entry, index) => (
-        <Selector
-          key={index}
-          character={entry.character}
-          onClick={selected => onClick(selected, entry)}
-        />
-      ))}
+      <div className="filter">
+        <p>Filter: </p>
+        <select value={view} onChange={handleChange}>
+          <option value="all">All</option>
+          <option value="freq">Hanzi Frequency</option>
+          <option value="hsk1">HSK 1</option>
+          <option value="hsk2">HSK 2</option>
+          <option value="hsk3">HSK 3</option>
+          <option value="hsk4">HSK 4</option>
+          <option value="hsk5">HSK 5</option>
+          <option value="hsk6">HSK 6</option>
+        </select>
+      </div>
+      <div className="view">
+        {filteredData.map((entry, index) => (
+          <Selector
+            key={index}
+            character={entry.character}
+            onClick={selected => onClick(selected, entry)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
